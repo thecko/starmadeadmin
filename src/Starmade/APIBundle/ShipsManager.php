7e572052928698493f2170d21e3e5e3acb7bef38
theck <jumptard.theck@gmail.com>
2015-01-13 09:39:29 +0100

    Started Abstract Starmade entity class and ship conversion

--- src/Starmade/APIBundle/ShipsManager.php
+++ src/Starmade/APIBundle/ShipsManager.php
@@ -5,111 +5,20 @@ namespace Starmade\APIBundle;
 use Symfony\Component\Security\Core\Util\SecureRandomInterface;
 use Starmade\APIBundle\Resources\SMDecoder;
 use Starmade\APIBundle\Model\Ship;
+use Starmade\APIBundle\StarmadeEntityManager;
 
-class ShipsManager {
-
-    /** @var array data */
-    protected $data = array();
-
-    /**
-     * @var \Symfony\Component\Security\Core\Util\SecureRandomInterface
-     */
-    protected $randomGenerator;
-
-    /**
-     * @var string
-     */
-    protected $cacheDir;
+class ShipsManager extends StarmadeEntityManager {
 
     public function __construct(SecureRandomInterface $randomGenerator, $cacheDir) {
-        $this->randomGenerator = $randomGenerator;
-        $this->cacheDir = $cacheDir;
-        
-        if (file_exists($cacheDir . '/sm_ship_data')) {
-            $data = file_get_contents($cacheDir . '/sm_ship_data');
-            $this->data = unserialize($data);
-        } else {
-            $this->data = $this->parseGameData();
-            $this->flush();
-        }
-
+        $this->type = "ship";
+        parent::__construct($randomGenerator, $cacheDir);
     }
 
-    private function flush() {
-        file_put_contents($this->cacheDir . '/sm_ship_data', serialize($this->data));
+    protected function getPrefix() {
+        return "ENTITY_SHIP_";
     }
 
-    public function fetch($start = 0, $limit = 5) {
-        return array_values( array_slice($this->data, $start, $limit, true) );
-    }
-
-    public function get($id) {
-        if (!isset($this->data[$id])) {
-            return false;
-        }
-
-        return $this->data[$id];
-    }
-
-    public function set($note) {
-        if (null === $note->id) {
-            if (empty($this->data)) {
-                $note->id = 0;
-            } else {
-                end($this->data);
-                $note->id = key($this->data) + 1;
-            }
-        }
-
-        if (null === $note->secret) {
-            $note->secret = base64_encode($this->randomGenerator->nextBytes(64));
-        }
-
-        $this->data[$note->id] = $note;
-        $this->flush();
-    }
-
-    public function remove($id) {
-        if (!isset($this->data[$id])) {
-            return false;
-        }
-
-        unset($this->data[$id]);
-        $this->flush();
-
-        return true;
-    }
-
-    protected function parseGameData() {
-        $smDecoder = new SMDecoder();
-
-//        $game_dir = $this->getContainer()->getParameter("starmade_dir");
-        $game_dir = "/data/xavi/areagamer/starmade/server/starmade/StarMade";
-
-        $prefix = "ENTITY_SHIP_"; //$this->getPrefix();
-        //exec( "ls " . STARMADE_DIR . "/server-database/DATA/ENTITY_SHIP_*" , $shipFiles );
-        $entityFiles = glob($game_dir . "/db/201412/" . $prefix . "*");
-        
-        $data = array();
-
-        if (!$entityFiles) {
-            return $data;
-        }
-
-
-        foreach ($entityFiles as $count => $entityFile) {
-            $entity = $this->createEntity($entityFile);
-            $data[$entity->uniqueid] = $entity;
-        }
-
-        return $data;
-    }
-
-    protected function createEntity($file) {
-        $decoder = new SMDecoder();
-
-        $shipEntity = $decoder->decodeSMFile($file);
-
+    protected function createEntity($shipEntity) {
         $name = $shipEntity["sc"]["realname"];
         $uniqueid = $shipEntity["sc"]["uniqueId"];
         $creatorid = $shipEntity["sc"]["creatoreId"];
@@ -117,4 +26,5 @@ class ShipsManager {
 
         return $ship;
     }
-} 
+
+}
---
+++ src/Starmade/APIBundle/StarmadeEntityManager.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Starmade\APIBundle;
+
+use Symfony\Component\Security\Core\Util\SecureRandomInterface;
+use Starmade\APIBundle\Resources\SMDecoder;
+use Starmade\APIBundle\Model\Ship;
+
+abstract class EntityManager {
+
+    /** @var array data */
+    protected $data = array();
+
+    /**
+     * @var \Symfony\Component\Security\Core\Util\SecureRandomInterface
+     */
+    protected $randomGenerator;
+
+    /**
+     * @var string
+     */
+    protected $cacheDir;
+    
+    /**
+     * @var string
+     */
+    protected $type;
+    
+    /**
+     * @var string
+     */
+    protected $file;
+
+    public function __construct(SecureRandomInterface $randomGenerator, $cacheDir) {
+        $this->randomGenerator = $randomGenerator;
+        $this->cacheDir = $cacheDir;
+        $this->file = '/sm_' . $this->type . '_data';
+        
+        if (file_exists($cacheDir . $this->file)) {
+            $data = file_get_contents($cacheDir . $this->file);
+            $this->data = unserialize($data);
+        } else {
+            $this->data = $this->parseGameData();
+            $this->flush();
+        }
+
+    }
+
+    private function flush() {
+        file_put_contents($this->cacheDir . $this->file, serialize($this->data));
+    }
+
+    public function fetch($start = 0, $limit = 5) {
+        return array_values( array_slice($this->data, $start, $limit, true) );
+    }
+
+    public function get($id) {
+        if (!isset($this->data[$id])) {
+            return false;
+        }
+
+        return $this->data[$id];
+    }
+
+    protected function parseGameData() {
+        $smDecoder = new SMDecoder();
+
+//        $game_dir = $this->getContainer()->getParameter("starmade_dir");
+        $game_dir = "/data/xavi/areagamer/starmade/server/starmade/StarMade";
+        $game_dir = "/home/theck/areagamer/starmade/server/starmade/StarMade";
+
+        $prefix = $this->getPrefix();
+        //exec( "ls " . STARMADE_DIR . "/server-database/DATA/ENTITY_SHIP_*" , $shipFiles );
+        $entityFiles = glob($game_dir . "/server-database/201412/" . $prefix . "*");
+        
+        $data = array();
+
+        if (!$entityFiles) {
+            return $data;
+        }
+
+        foreach ($entityFiles as $count => $entityFile) {
+            $decoder = new SMDecoder();    
+            $entityData = $decoder->decodeSMFile($entityFile);
+            $entity = $this->createEntity($entityData);
+            $data[$entity->uniqueid] = $entity;
+        }
+
+        return $data;
+    }
+    
+    /**
+     * The entity file will start with this prefix
+     */
+    protected abstract function getPrefix();
+
+    /**
+     * Constructs the object
+     */
+    protected abstract function createEntity($file);
+
+
+} 
